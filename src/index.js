917c50fa711b1287c81177b8d280f4b284638926
MaxStarovoitov <rackezik@gmail.com>
2018-03-01 15:47:53 +0300

    Solution Money-Exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-// PLEASE DON'T change function name
+function remainder(a, b) { return (a - a % b) / b; }
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+  var result = {};
+
+  if (currency >= 10000) { result.error = "You are rich, my friend! We don't have so much coins for exchange"; }
+  else {
+    H = remainder(currency, 50);
+    Q = remainder((currency % 50),25);
+    D = remainder((currency % 50)%25,10);
+    N = remainder(((currency % 50)%25)%10,5);
+    P = remainder((((currency % 50)%25)%10)%5,1);
+
+    result.H = ((H > 0) ? H : 0);
+    result.Q = ((Q > 0) ? Q : 0);
+    result.D = ((D > 0) ? D : 0);
+    result.N = ((N > 0) ? N : 0);
+    result.P = ((P > 0) ? P : 0);
+  }
+
+  return result;
+};
