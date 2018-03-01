43e72082a6b23d35d78fa61faaafeaef084a6a5f
MaxStarovoitov <rackezik@gmail.com>
2018-03-01 16:38:53 +0300

    solution2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 module.exports = function makeExchange(currency) {
   var result = new Object();
 
-  if (currency > 10000 ) { result.error = "You are rich, my friend! We don't have so much coins for exchange"; }
+  if (currency >= 10000 ) { result.error = "You are rich, my friend! We don't have so much coins for exchange"; }
   else {
-    H = remainder(currency, 50);
-    Q = remainder((currency % 50),25);
-    D = remainder((currency % 50)%25,10);
-    N = remainder(((currency % 50)%25)%10,5);
-    P = remainder((((currency % 50)%25)%10)%5,1);
+  H = (currency - currency % 50) / 50;
+  Q = ((currency % 50) - (currency % 50) % 25) / 25;
+  D = (((currency % 50) % 25) - ((currency % 50) % 25) % 10) / 10;
+  N = ((((currency % 50) % 25) % 10) - (((currency % 50) % 25) % 10) % 5) / 5;
+  P = (((((currency % 50) % 25) % 10) % 5) - ((((currency % 50) % 25) % 10) % 5) % 1) / 1;
 
-    if (H > 0) { o.H = H; }
-    if (Q > 0) { o.Q = Q; }
-    if (D > 0) { o.D = D; }
-    if (N > 0) { o.N = N; }
-    if (P > 0) { o.P = P; }
+
+  if (H > 0) { result.H = H; }
+  if (Q > 0) { result.Q = Q; }
+  if (D > 0) { result.D = D; }
+  if (N > 0) { result.N = N; }
+  if (P > 0) { result.P = P; }
   }
 
   return result;
 };
-
-function remainder(a, b){ return (a - a % b) / b; }
