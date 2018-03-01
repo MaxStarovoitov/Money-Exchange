2292e12e3675291d1ab3c767faf5d1f28d880793
MaxStarovoitov <rackezik@gmail.com>
2018-03-01 16:00:27 +0300

    addition

--- src/index.js
+++ src/index.js
@@ -1,22 +1,58 @@
-function remainder(a, b) { return (a - a % b) / b; }
-
 module.exports = function makeExchange(currency) {
-  var result = {};
+  var h,
+    q,
+    d,
+    n,
+    p,
+    result = 0,
+    query = { 'H': 0, 'Q': 0, 'D': 0, 'N': 0, 'P': 0 },
+    msg = [];
+    msg.false = 'You are rich, my friend! We dont have so much coins for exchange';
 
-  if (currency >= 10000) { result.error = "You are rich, my friend! We don't have so much coins for exchange"; }
-  else {
-    H = remainder(currency, 50);
-    Q = remainder((currency % 50),25);
-    D = remainder((currency % 50)%25,10);
-    N = remainder(((currency % 50)%25)%10,5);
-    P = remainder((((currency % 50)%25)%10)%5,1);
+  if (currency >= 10000) { return(msg); }
 
-    result.H = ((H > 0) ? H : 0);
-    result.Q = ((Q > 0) ? Q : 0);
-    result.D = ((D > 0) ? D : 0);
-    result.N = ((N > 0) ? N : 0);
-    result.P = ((P > 0) ? P : 0);
-  }
 
-  return result;
+  h = Math.floor(currency/50);
+
+  if (h > 0){
+        query.H = h;
+    }
+
+    result = currency % 50;
+
+
+    q = Math.floor(result/25);
+
+    if(q > 0){
+        query.Q = q;
+    }
+
+    result = result % 25;
+
+
+    d = Math.floor(result/10);
+
+    if(d > 0){
+        query.D = d;
+    }
+
+    result = result % 10;
+
+
+    n = Math.floor(result/5);
+
+    if(n > 0){
+        query.N = n;
+    }
+
+    result = result % 5;
+
+    p = result%5;
+
+    if(p > 0){
+        query.P = p;
+    }
+
+
+  return(query);
 };
